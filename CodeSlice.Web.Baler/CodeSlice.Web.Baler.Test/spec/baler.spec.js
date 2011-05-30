a1711306420c34608cb8c88f6139793cd0560fe6
James Hughes <kouphax@gmail.com>
2011-05-30 20:19:15 +0100

    First Commit of CoffeeScript Extension

---
+++ CodeSlice.Web.Baler/CodeSlice.Web.Baler.Test/spec/baler.spec.js
@@ -0,0 +1,6 @@
+﻿describe("Bale Single File", function(){
+    it("should successfuly bale a single coffeescript file into javascript without changing the behaviour", function(){
+        expect(window.coffeescriptsingle).toBeDefined();
+        expect(window.coffeescriptsingle()).toBe('coffeescript-single');
+    });
+});
